d955973a44a899eb31a1ac5cf9958f333047347c
Pasha-Akito <1masterpasha@gmail.com>
2021-02-01 19:23:19 +0000

    Basics

---
+++ basics.py
@@ -0,0 +1,16 @@
+# Python Basics
+print("Hello world")
+
+a = 1
+b = 1.0
+s = "hello from 'a' string"
+t = '"hello" from a different string'
+
+print(a, b, s, t)
+
+print(type(s))
+
+print(s[0:2]) #prints he. substring(0,2)
+
+print(s[3:10:1]) #for(int i = 3; i < 10; i++) return i;
+
