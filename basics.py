4aca58faa733b00e118361c6d9e5c60794a09ccf
Pasha-Akito <1masterpasha@gmail.com>
2021-02-12 16:40:27 +0000

    Lists and dictionaries

--- basics.py
+++ basics.py
@@ -14,3 +14,30 @@ print(s[0:2]) #prints he. substring(0,2)
 
 print(s[3:10:1]) #for(int i = 3; i < 10; i++) return i;
 
+x = [1, 2, 3, "Hello", 1.0]
+print(x) # [1, 2, 3, 'Hello']
+#print(x[0])
+#print(x[2])
+#print(x[-1])
+
+for i in x[::2]:
+    print(i)
+    print(i + i)
+
+
+#for i in range(10):
+#    print(i)
+
+d = {"no_wheels": 4, "make": "Skoda"}
+print(d["no_wheels"])
+print(d["make"])
+
+d["model"] = "Superb"
+
+print(d["model"])
+
+r = [1,2,3,4]
+print(r)
+
+s = [i * i for i in r]
+print(s)
